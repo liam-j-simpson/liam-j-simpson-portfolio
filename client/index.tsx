b4f2010c63d23ed9ab24808054fee99d3bb6ce48
Liam Simpson <contact@liamsimpson.co.nz>
2025-01-07 18:45:20 +1300

    creating api functions initial structure

--- client/index.tsx
+++ client/index.tsx
@@ -1,9 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <RouterProvider router={router} />,
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />,
+    </QueryClientProvider>,
   )
 })
