640ca25567cc098e514cf6e949d7678ca65709d2
Liam Simpson <contact@liamsimpson.co.nz>
2025-01-09 10:38:48 +1300

    completed api functions

--- client/api.ts
+++ client/api.ts
@@ -1,3 +1,4 @@
+import { Project } from 'models/projects'
 import request from 'superagent'
 
 //Get All Projects
@@ -6,50 +7,44 @@ export async function getAllProjects() {
     const res = await request.get('/api/v1/projects')
     return res.body.projects
   } catch (error) {
-    console.error({
-      message: error?.message,
-      //here last, working on error messages with superagent.
-    })
-    throw {
-      
-      status:
-    }
-    
+    console.error(error)
   }
 }
 
 //Get Projects By ID
-export async function getProjectById() {
+export async function getProjectById(id: number) {
   try {
-    
+    const res = await request.get(`/api/v1/projects/${id}`)
+    return res.body.project
   } catch (error) {
-    
+    console.error(error)
   }
 }
 
 //Add Project
-export async function addProject() {
+export async function addProject(project: Project) {
   try {
-    
+    const res = await request.post('/api/v1/projects/').send(project)
+    return res.body
   } catch (error) {
-    
+    console.error(error)
   }
 }
 
 //Delete Project
-export async function deleteProject() {
+export async function deleteProject(id: number) {
   try {
-    
+    await request.delete(`/api/v1/projects/${id}`)
   } catch (error) {
-    
+    console.error(error)
   }
 }
 
 //Edit Project
-export async function editProject() {
+export async function editProject(id: number, changes: Project) {
   try {
-    
+    await request.patch(`/api/v1/projects/${id}`).send(changes)
   } catch (error) {
-    
+    console.error(error)
   }
 }
