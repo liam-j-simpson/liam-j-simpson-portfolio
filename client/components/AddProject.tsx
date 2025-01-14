09ea144bb6b585d7c6fde10cfcc30ee6f076a4a5
Liam Simpson <contact@liamsimpson.co.nz>
2025-01-14 13:44:55 +1300

    creating dumb components for admin page

---
+++ client/components/AddProject.tsx
@@ -0,0 +1,32 @@
+import { Label } from '@radix-ui/react-label'
+import { Button } from '../shadcn/Button'
+import { Input } from '../shadcn/Input'
+
+export function AddProject() {
+  return (
+    <form>
+      <h2 className="text-s">Add Project</h2>
+      <p className="mb-2.5">Fill out the form below to add a project.</p>
+
+      <Label htmlFor="projectName">Project Name</Label>
+      <Input id="projectName" placeholder="Enter project name"></Input>
+
+      <Label htmlFor="shortDescription">Short Description</Label>
+      <Input
+        id="shortDescription"
+        placeholder="Enter short description"
+      ></Input>
+
+      <Label htmlFor="longDescription">Long Description</Label>
+      <Input id="longDescription" placeholder="Enter long description"></Input>
+
+      <Label htmlFor="stack">Stack</Label>
+      <Input id="stack" placeholder="Press enter to add stack tag"></Input>
+
+      <Label htmlFor="date">Date</Label>
+      <Input id="date" placeholder="Date"></Input>
+
+      <Button type="submit">Submit</Button>
+    </form>
+  )
+}
--- client/components/pages/Admin.tsx
+++ client/components/pages/Admin.tsx
@@ -1,43 +1,19 @@
-import { Label } from '@radix-ui/react-label'
-import { Button } from '../../shadcn/Button'
-import { Input } from '../../shadcn/Input'
+import { AddProject } from '../AddProject'
+import { ViewProject } from '../ViewProject'
+
 export function Admin() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
   return (
     <>
       <h1 className="text-xl">ADMIN</h1>
       <div>
-        <h2 className="text-s">Current Projects</h2>
+        <ViewProject />
       </div>
-      
-      <form>
-        <h2 className="text-s">Create New Project</h2>
-        <p className="mb-2.5">
-          Fill out the form below to create a new project.
-        </p>
 
-        <Label htmlFor="projectName">Project Name</Label>
-        <Input id="projectName" placeholder="Enter project name"></Input>
-
-        <Label htmlFor="shortDescription">Short Description</Label>
-        <Input
-          id="shortDescription"
-          placeholder="Enter short description"
-        ></Input>
-
-        <Label htmlFor="longDescription">Long Description</Label>
-        <Input
-          id="longDescription"
-          placeholder="Enter long description"
-        ></Input>
-
-        <Label htmlFor="stack">Stack</Label>
-        <Input id="stack" placeholder="Press enter to add stack tag"></Input>
-
-        <Label htmlFor="date">Date</Label>
-        <Input id="date" placeholder="Date"></Input>
-
-        <Button type="submit">Submit</Button>
-      </form>
+      <AddProject onSubmit={handleSubmit} />
+      {/* PROP DRILLING THE ONSUBMIT FUNCTION */}
     </>
   )
 }
