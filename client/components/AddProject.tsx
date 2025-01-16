f72f329fdad289432e3654f566d8b56b147527d4
Liam Simpson <contact@liamsimpson.co.nz>
2025-01-16 18:39:13 +1300

    creating form for adding projects

--- client/components/AddProject.tsx
+++ client/components/AddProject.tsx
@@ -1,24 +1,65 @@
 import { Label } from '@radix-ui/react-label'
 import { Button } from '../shadcn/Button'
 import { Input } from '../shadcn/Input'
+import { useState } from 'react'
 
 export function AddProject() {
+  const [form, setForm] = useState({
+    name: '',
+    description: '',
+    stack: [],
+    date: '',
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = () => {}
+
+  const handleChangeDate = (e) => {
+    const { name, value } = e.target
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleChangeTags = () => {}
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2 className="text-s">Add Project</h2>
       <p className="mb-2.5">Fill out the form below to add a project.</p>
 
-      <Label htmlFor="projectName">Project Name</Label>
-      <Input id="projectName" placeholder="Enter project name"></Input>
+      <Label htmlFor="name">Project Name</Label>
+      <Input
+        id="name"
+        name="name"
+        placeholder="Enter project name"
+        onChange={handleChange}
+      ></Input>
 
       <Label htmlFor="description">Description</Label>
-      <Input id="description" placeholder="Enter description"></Input>
+      <Input
+        id="description"
+        name="description"
+        placeholder="Enter description"
+        onChange={handleChange}
+      ></Input>
 
       <Label htmlFor="stack">Stack</Label>
-      <Input id="stack" placeholder="Press enter to add stack tag"></Input>
+      <Input
+        id="stack"
+        name="stack"
+        placeholder="Press enter to add stack tag"
+        onChange={handleChangeTags}
+      ></Input>
 
       <Label htmlFor="date">Date</Label>
-      <Input id="date" placeholder="Date"></Input>
+      <Input
+        id="date"
+        name="date"
+        placeholder="Date"
+        onChange={handleChangeDate}
+      ></Input>
 
       <Button type="submit">Submit</Button>
     </form>
