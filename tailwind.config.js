83fae6a9f60d58ae03e4f2fe3b7faa3525e67025
Liam Simpson <contact@liamsimpson.co.nz>
2025-01-21 11:19:16 +1300

    Moved input and output css fie to src folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ['class'],
-    content: ['./client/components/**/*.tsx', './index.html'],
+  darkMode: ['class'],
+  content: ['./client/components/**/*.tsx', './index.html'],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			larken: [
-  				'larken',
-  				'serif'
-  			],
-  			inconsolata: [
-  				'inconsolata',
-  				'san-serif'
-  			]
-  		},
-  		fontSize: {
-  			s: '1.2rem',
-  			l: '5rem',
-  			xl: '18rem'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      fontFamily: {
+        larken: ['larken', 'serif'],
+        inconsolata: ['inconsolata', 'san-serif'],
+      },
+      fontSize: {
+        s: '1.2rem',
+        l: '5rem',
+        xl: '18rem',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      colors: {},
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 }
