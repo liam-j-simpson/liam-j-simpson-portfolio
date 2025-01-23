3c46a66eab44447fc305e6d8e7244df467e50804
Liam Simpson <contact@liamsimpson.co.nz>
2025-01-23 17:17:21 +1300

    created project page

--- client/components/pages/ProjectPage.tsx
+++ client/components/pages/ProjectPage.tsx
@@ -1,13 +1,34 @@
+import { useParams } from 'react-router-dom'
+import { useGetProject } from '../hooks/useGetProject'
+
 export function ProjectPage() {
-  return (
-    <>
-      <h1 className="text-xl">Project</h1>
-      <h2 className="text-s mb-3">aaa</h2>
-      <p className="mb-3">aaa</p>
-
-      <h2 className="text-s mb-3">aaa</h2>
-
-      <p className="mb-3">aaa</p>
-    </>
-  )
+  const { id } = useParams()
+  const { isPending, isError, error, data } = useGetProject(Number(id))
+  console.log(data)
+  if (isPending) {
+    return <h2>Loading...</h2>
+  }
+  if (isError) {
+    return <h2>Error: {error.message}</h2>
+  }
+  if (data) {
+    return (
+      <>
+        <h1 className="text-xl">{data.name}</h1>
+        <p className="mb-3">{data.date}</p>
+        <ul className="flex">
+          {data.tags.map((item: string) => (
+            <li key={item}>
+              <button className="rounded-full px-6 outline mb-3 mr-3">
+                {item}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <h2 className="text-s mb-3">{data.description}</h2>
+        <p className="mb-3">body copy</p>
+        <h1 className="text-xl">image gallery</h1>
+      </>
+    )
+  }
 }
