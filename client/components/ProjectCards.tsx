8bd5cacbd097de7031167a592a192c10bd91cc27
Liam Simpson <contact@liamsimpson.co.nz>
2025-01-23 17:17:21 +1300

    created project page

--- client/components/ProjectCards.tsx
+++ client/components/ProjectCards.tsx
@@ -1,4 +1,5 @@
 import { ProjectArray, ProjectData } from 'models/projects'
+import { Link } from 'react-router-dom'
 
 export function ProjectCards({ data }: ProjectArray) {
   return (
@@ -6,19 +7,21 @@ export function ProjectCards({ data }: ProjectArray) {
       <ul>
         {data.map((item: ProjectData) => (
           <li key={item.id}>
-            <h2 className="text-s mb-3">{item.name}</h2>
+            <Link to={`/${item.id}`}>
+              <h2 className="text-s mb-3">{item.name}</h2>
 
-            <ul className="flex">
-              {item.tags.map((item) => (
-                <li key={item}>
-                  <button className="rounded-full px-6 outline mb-3 mr-3">
-                    {item}
-                  </button>
-                </li>
-              ))}
-            </ul>
+              <ul className="flex">
+                {item.tags.map((item) => (
+                  <li key={item}>
+                    <button className="rounded-full px-6 outline mb-3 mr-3">
+                      {item}
+                    </button>
+                  </li>
+                ))}
+              </ul>
 
-            <p className="mb-3">{item.description}</p>
+              <p className="mb-3">{item.description}</p>
+            </Link>
           </li>
         ))}
       </ul>
