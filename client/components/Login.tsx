b80e6988fd79652698b358102f632f8dac1270b1
Liam Simpson <contact@liamsimpson.co.nz>
2025-01-29 12:54:08 +1300

    using auth provider with history ive got the authentication working

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -2,7 +2,16 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 export function Login() {
   const { loginWithRedirect, isAuthenticated } = useAuth0()
+
+  const handleLogin = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: '/admin',
+      },
+    })
+  }
+
   if (!isAuthenticated) {
-    return <button onClick={() => loginWithRedirect()}>Log In</button>
+    return <button onClick={handleLogin}>Log In</button>
   }
 }
--- client/index.tsx
+++ client/index.tsx
@@ -2,27 +2,16 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Auth0Provider } from '@auth0/auth0-react'
+import { Auth0ProviderWithHistory } from './components/Auth0ProviderWithHistory'
 
 const queryClient = new QueryClient()
-const domain = import.meta.env.VITE_AUTH0_DOMAIN
-const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
-const audience = import.meta.env.VITE_AUTH0_AUDIENCE
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <Auth0Provider
-      domain={`${domain}`}
-      clientId={`${clientId}`}
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-        audience: audience,
-      }}
-    >
+    <Auth0ProviderWithHistory>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
-      ,
-    </Auth0Provider>,
+    </Auth0ProviderWithHistory>,
   )
 })
