e61c1a7e37e8d824b637352d6c06135734391637
Liam Simpson <contact@liamsimpson.co.nz>
2025-01-29 13:41:40 +1300

    refactoring auth0provider, removed auth0providerwithhistory

--- client/index.tsx
+++ client/index.tsx
@@ -2,16 +2,26 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Auth0ProviderWithHistory } from './components/authentication/Auth0ProviderWithHistory'
-
+import { Auth0Provider } from '@auth0/auth0-react'
 const queryClient = new QueryClient()
+const domain = import.meta.env.VITE_AUTH0_DOMAIN
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
+const audience = import.meta.env.VITE_AUTH0_AUDIENCE
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <Auth0ProviderWithHistory>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: audience,
+      }}
+      onRedirectCallback={() => router.navigate('/admin')}
+    >
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
-    </Auth0ProviderWithHistory>,
+    </Auth0Provider>,
   )
 })
