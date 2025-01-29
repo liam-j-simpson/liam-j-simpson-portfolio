0e120430a49f14a88c1b8c1d6d555862dc31375d
Liam Simpson <contact@liamsimpson.co.nz>
2025-01-29 14:31:07 +1300

    adding PageLoader component to all pending/loading states. Deleting Login component.

--- client/components/authentication/AuthenticationGuard.tsx
+++ client/components/authentication/AuthenticationGuard.tsx
@@ -1,11 +1,11 @@
 import { withAuthenticationRequired } from '@auth0/auth0-react'
 import { PageLoader } from './PageLoader'
 
-interface AuthenticationGuardProps {
+interface Props {
   component: React.ComponentType
 }
 
-export function AuthenticationGuard({ component }: AuthenticationGuardProps) {
+export function AuthenticationGuard({ component }: Props) {
   const Component = withAuthenticationRequired(component, {
     onRedirecting: () => <PageLoader />,
   })
--- client/components/pages/Projects.tsx
+++ client/components/pages/Projects.tsx
@@ -1,10 +1,11 @@
+import { PageLoader } from '../authentication/PageLoader'
 import { useGetAllProjects } from '../hooks/useGetProject'
 import { ProjectCards } from '../ProjectCards'
 
 export function Projects() {
   const { isPending, isError, error, data } = useGetAllProjects()
   if (isPending) {
-    return <h2>Loading...</h2>
+    return <PageLoader/>
   }
   if (isError) {
     return <h2>Error: {error.message}</h2>
