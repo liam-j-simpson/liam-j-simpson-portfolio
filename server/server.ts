1e3d99f3a7cdf5e36e9a860a26c886fb448e8cd2
Liam Simpson <contact@liamsimpson.co.nz>
2025-02-05 11:23:23 +1300

    changing server.ts config

--- server/server.ts
+++ server/server.ts
@@ -5,12 +5,11 @@ import projectsRoutes from './routes/projects'
 const server = express()
 
 server.use(express.json())
-server.use(express.static('public'))
-
 server.use('/api/v1/projects', projectsRoutes)
 
+server.use(express.static(Path.resolve('public')))
+
 if (process.env.NODE_ENV === 'production') {
-  server.use(express.static(Path.resolve('public')))
   server.use('/assets', express.static(Path.resolve('./dist/assets')))
   server.get('*', (req, res) => {
     res.sendFile(Path.resolve('./dist/index.html'))
