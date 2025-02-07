5aaada5d7c45f891f0a61ba571ad8679f2f4882f
Liam Simpson <contact@liamsimpson.co.nz>
2025-02-07 13:00:10 +1300

    deployment settings

--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,11 @@ import projectsRoutes from './routes/projects'
 const server = express()
 
 server.use(express.json())
+server.use(express.urlencoded({ extended: false }))
+
 server.use('/api/v1/projects', projectsRoutes)
+
+server.use(express.static(Path.resolve('public')))
 
 if (process.env.NODE_ENV === 'production') {
   server.use('/assets', express.static(Path.resolve('./dist/assets')))
