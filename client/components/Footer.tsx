f9789bdcb68318e6e05015d981884f99afa306a8
Liam Simpson <contact@liamsimpson.co.nz>
2025-02-10 12:37:48 +1300

    updating footer with new header

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,7 +1,8 @@
 export function Footer() {
   return (
     <footer className="mx-10 my-3 border-black border-t-2">
-      <h2 className="text-s">Liam Simpson</h2>
+      <h2 className="text-s">Contact</h2>
+      
     </footer>
   )
 }
