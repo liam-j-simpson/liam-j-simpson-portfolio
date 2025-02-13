46ea6805f2019cef1ab39ca9cdd35fb4c060ecca
Liam Simpson <contact@liamsimpson.co.nz>
2025-02-14 12:25:20 +1300

    Removing knex and sql packages

---
+++ server/db/seed.ts
@@ -0,0 +1,24 @@
+{
+  id: 1,
+  name: 'PAKT',
+  date: '2025-01-17',
+  summary: 'PAKT is for outdoors people',
+  description:
+    'A common problem for outdoors people is that they love adventure but due to other commitments are time-poor and dont get to enjoy their passions as much as theyd hope.',
+  url: 'https://www.pakt.co.nz',
+  tags: JSON.stringify(['React', 'Typescript', 'Tailwind']),
+  thumbnail: 'uploads/monarch.png',
+  gallery: JSON.stringify(['uploads/monarch2.png', 'uploads/monarch.png']),
+},
+{
+  id: 2,
+  name: 'Circa',
+  date: '2025-01-18',
+  summary: 'Circas new website',
+  description:
+    'Circa likes to stand out of the crowd as the agency thats purely focussed on its customers. Using fun imagery and videos of past clients plus colourful grids that representive each properties unique style. This website gives personality to an industry that often lacks it.',
+  url: 'https://www.circarealestate.co.nz/',
+  tags: JSON.stringify(['Webflow']),
+  thumbnail: 'uploads/monarch2.png',
+  gallery: JSON.stringify(['uploads/monarch2.png', 'uploads/monarch.png']),
+},
