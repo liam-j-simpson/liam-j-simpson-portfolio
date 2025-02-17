402297c949b1d1401c4ea97738d5d054063c6b67
Liam Simpson <contact@liamsimpson.co.nz>
2025-02-17 18:38:49 +1300

    changing server config

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,13 @@
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 import server from './server.ts'
 
-dotenv.config()
-const port = process.env.PORT || 3000
+// dotenv.config()
+// const port = process.env.PORT || 3000
 
-server.get('/', (req, res) => {
-  res.send('Server is running')
-})
 
-server.listen(port, () => {
-  console.log(
-    `Environment: ${process.env.NODE_ENV}, listening on port: ${port}.`,
-  )
-})
+
+// server.listen(port, () => {
+//   console.log(
+//     `Environment: ${process.env.NODE_ENV}, listening on port: ${port}.`,
+//   )
+// })
--- server/server.ts
+++ server/server.ts
@@ -6,16 +6,18 @@ import dotenv from 'dotenv'
 dotenv.config()
 const server = express()
 
-
 server.use(express.json())
 
 server.use('/api/v1/projects', projectsRoutes)
 
 server.use(express.static(Path.resolve('public')))
 
+server.get('/', (req, res) => {
+  res.send('Server is running')
+})
+
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static('./dist/client'))
-  
 }
 
 export default server
