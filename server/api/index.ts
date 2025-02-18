23b131962c50fd97d09eb716978a65d1fd0c69c5
Liam Simpson <contact@liamsimpson.co.nz>
2025-02-19 12:02:16 +1300

    uncommenting projectRoutes in index file

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,12 +1,12 @@
 import express from 'express'
 // import * as Path from 'node:path'
-// import projectsRoutes from './routes/projects'
+import projectsRoutes from './routes/projects'
 
 const app = express()
 
 // app.use(express.json())
 
-// app.use('/api/v1/projects', projectsRoutes)
+app.use('/api/v1/projects', projectsRoutes)
 
 // app.use(express.static(Path.resolve('public')))
 
