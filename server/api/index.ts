735e1e6400c2d721bb71426e2dc41d3f046e8336
Liam Simpson <contact@liamsimpson.co.nz>
2025-02-19 12:55:05 +1300

    changing package scripts

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import * as Path from 'node:path'
-import { getAllProjects, getProjectById } from './db/projects'
-// import projectsRoutes from './routes/projects'
+import projectsRoutes from './routes/projects'
 
 const server = express()
 
@@ -13,28 +12,7 @@ server.get('/', (req, res) => {
   res.send('Express on Vercel')
 })
 
-// server.use('/api/v1/projects', projectsRoutes)
-
-// GET ALL PROJECTS
-server.get('/all', async (_req, res, next) => {
-  try {
-    const projects = await getAllProjects()
-    res.json({ projects })
-  } catch (error) {
-    next(error)
-  }
-})
-
-// GET ONE PROJECT
-server.get('/:id', async (req, res, next) => {
-  try {
-    const id = req.params.id
-    const project = await getProjectById(id)
-    res.json({ project })
-  } catch (error) {
-    next(error)
-  }
-})
+server.use('/api/v1/projects', projectsRoutes)
 
 server.listen(3000, () => {
   console.log('Server ready on port 3000')
--- server/api/middleware/checkPermissions.ts
+++ server/api/middleware/checkPermissions.ts
@@ -2,14 +2,16 @@ import { NextFunction, Request, Response } from 'express'
 
 export const checkPermissions = (requiredPermission: string) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const userPermissions = req.auth?.payload.permissions as string[]
-    if (userPermissions.includes(requiredPermission)) {
-      return next()
-    } else {
-      res.status(403).json({
-        error: 'Incorrect Permissions',
-      })
-      return
+    {
+      const userPermissions = req.auth?.payload.permissions as string[]
+      if (userPermissions.includes(requiredPermission)) {
+        return next()
+      } else {
+        res.status(403).json({
+          error: 'Incorrect Permissions',
+        })
+        return
+      }
     }
   }
 }
