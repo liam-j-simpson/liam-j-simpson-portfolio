f8be0da82529f10939bf4e18ddc9b10bbf4e8a82
Liam Simpson <contact@liamsimpson.co.nz>
2025-03-01 15:41:22 +1300

    updating index variables to not be hard coded

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import app from './server'
 
 const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('Server ready on port', PORT)
 })
