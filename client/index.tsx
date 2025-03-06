42179fabc1ef922ac0a60192cb1167a85e17d01e
Liam Simpson <contact@liamsimpson.co.nz>
2025-03-07 09:28:41 +1300

    changing console logs

--- client/index.tsx
+++ client/index.tsx
@@ -6,7 +6,7 @@ import { Auth0Provider } from '@auth0/auth0-react'
 const router = createBrowserRouter(routes)
 const queryClient = new QueryClient()
 
-const domain = import.meta.env.VITE_AUTH0_DOMAIN
+const domain = process.env.VITE_AUTH0_DOMAIN
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
 const audience = import.meta.env.VITE_AUTH0_AUDIENCE
 console.log('domain', domain)
