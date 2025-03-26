45290ec0368a3d0f8c5fdc25151fdea7af2d060b
Liam Simpson <contact@liamsimpson.co.nz>
2025-03-27 10:04:12 +1300

    removing header in footer component

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -5,24 +5,16 @@ export function Footer() {
     <>
       <div className="grid grid cols-1 border-black border-t-2 md:grid-cols-2 gap-12 py-6">
         <div>
-          <h2 className="~text-hs/hxs mb-1 lg:mb-3">
-            Lets work together, get in touch.
-          </h2>
-          <p className="~text-ps">+64211855651</p>
+          <p>+64211855651</p>
           <p>liamsimpsondesign@gmail.com</p>
         </div>
-
-        <div className="flex-col mt-1 gap-x-3">
-          <div>
-            <NavLink to="/">
-              <p>Projects</p>
-            </NavLink>
-          </div>
-          <div>
-            <NavLink to="/contact">
-              <p>Contact</p>
-            </NavLink>
-          </div>
+        <div className="mt-1 gap-x-3">
+          <NavLink to="/">
+            <p>Projects</p>
+          </NavLink>
+          <NavLink to="/contact">
+            <p>Contact</p>
+          </NavLink>
         </div>
       </div>
     </>
