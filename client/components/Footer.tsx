68a79e41cb214eb818dc6f283642721407247160
Liam Simpson <contact@liamsimpson.co.nz>
2025-03-27 10:17:33 +1300

    adding h2 back into the footer

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -3,7 +3,8 @@ import { NavLink } from 'react-router-dom'
 export function Footer() {
   return (
     <>
-      <div className="grid grid cols-1 border-black border-t-2 md:grid-cols-2 gap-12 py-6">
+      <h2 className="~text-hs/hxs mb-1 pt-6 border-black border-t-2 lg:mb-3">{`Let's work together, get in touch.`}</h2>
+      <div className="grid grid cols-1 md:grid-cols-2 gap-12 pb-6">
         <div>
           <p>+64211855651</p>
           <p>liamsimpsondesign@gmail.com</p>
