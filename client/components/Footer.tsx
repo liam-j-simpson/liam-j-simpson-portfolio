224d8f1175c7911f1c46787f9c76b801353b7431
Liam Simpson <contact@liamsimpson.co.nz>
2025-03-28 12:51:26 +1300

    tightening up styling of footer element.

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -3,8 +3,10 @@ import { NavLink } from 'react-router-dom'
 export function Footer() {
   return (
     <>
-      <h2 className="~text-hs/hxs mb-1 pt-6 border-black border-t-2 lg:mb-3">{`Let's work together, get in touch.`}</h2>
-      <div className="grid grid cols-1 md:grid-cols-2 gap-12 pb-6">
+      <h2 className="~text-hs/hxs pt-6 border-black border-t-2">{`Let's work together,`}</h2>
+      <h2 className="~text-hs/hxs mb-1 lg:mb-3">get in touch.</h2>
+
+      <div className="grid grid cols-1 md:grid-cols-2 gap-6 pb-6">
         <div>
           <p>+64211855651</p>
           <p>liamsimpsondesign@gmail.com</p>
