708bb69016c892e6590a8c14a074c3ed3c0c3e40
Liam Simpson <contact@liamsimpson.co.nz>
2025-03-30 15:30:46 +1300

    updating footer contact to match Contact page

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -3,18 +3,48 @@ import { NavLink } from 'react-router-dom'
 export function Footer() {
   return (
     <>
-      <h2 className="~text-hs/hxs pt-6 border-black border-t">
+      <h2 className="~text-hs/hxs pt-6 pb-3 border-black border-t">
         {`LET'S WORK TOGETHER`}
       </h2>
       <div className="grid grid cols-1 md:grid-cols-2 gap-6 pb-6">
         <div>
-          <p>+64211855651</p>
-          <p>liamsimpsondesign@gmail.com</p>
+          <div className="flex flex-wrap pb-1.5">
+            <button
+              className="flex pr-3 underline"
+              onClick={() => navigator.clipboard.writeText('+64211855651')}
+            >
+              <p>+64211855651 </p>
+            </button>
+
+            <button className="rounded-full outline outline-1 px-3">
+              <a href="tel:+64211855651">
+                <p>Call</p>
+              </a>
+            </button>
+          </div>
+
+          <div className="flex flex-wrap">
+            <button
+              className="flex pr-3 underline"
+              onClick={() =>
+                navigator.clipboard.writeText('liamsimpsondesign@gmail.com')
+              }
+            >
+              <p className="underline">liamsimpsondesign@gmail.com</p>
+            </button>
+            <button className="rounded-full outline outline-1 px-3">
+              <a href="mailto:liamsimpsondesign@gmail.com">
+                <p>Email</p>
+              </a>
+            </button>
+          </div>
         </div>
-        <div className="mt-1 gap-x-3">
-          <NavLink to="/">
+
+        <div className="flex flex-col">
+          <NavLink to="/" className="pb-1.5">
             <p>Projects</p>
           </NavLink>
+
           <NavLink to="/contact">
             <p>Contact</p>
           </NavLink>
